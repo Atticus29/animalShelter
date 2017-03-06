8eff23758c0d993d74cc48845cf6d3bfc528c93f
Mark Fisher and Oliver Fu <oliver@>
2017-03-06 11:32:25 -0800

    add front end logic to hide divs associated with animals that have already been adopted

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,18 @@ function Animal(name, adoptionStatus){
 }
 
 // Front End
+$(function(){
+  var rhino = new Animal(rhino, false);
+  var gorilla = new Animal(gorilla, true);
+  var bat = new Animal(bat, false);
+
+  if (rhino.adoptionStatus === true){
+    $("#rhino").hide();
+  }
+  if (gorilla.adoptionStatus === true){
+    $("#gorilla").hide();
+  }
+  if (bat.adoptionStatus === true){
+    $("#bat").hide();
+  }
+});
