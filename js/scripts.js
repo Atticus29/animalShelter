73e72397eb1dec7437d94d3df2a63391dc7a74eb
chance magno and jahan walsh <chance@>
2017-03-06 13:50:59 -0800

    add gif to jumbotron, center recently adopted text, add css styles.

--- js/scripts.js
+++ js/scripts.js
@@ -32,18 +32,18 @@ $(function(){
     if(buttonID === "rhinoBtn"){
       console.log(rhino.adoptionStatus);
       rhino.adoptionStatus = true;
-      $("#rambo-adopted").show();
+      $("#rambo-adopted").slideDown();
       $("#recent-adopted").show();
       // console.log(rhino);
       console.log(rhino.adoptionStatus);
     } else if (buttonID === "gorillaBtn"){
       gorilla.adoptionStatus = true;
-      $("#harambe-adopted").show();
-      $("#rambo-adopted").show();
+      $("#harambe-adopted").slideDown();
+      $("#recent-adopted").show();
     } else if (buttonID === "batBtn") {
       bat.adoptionStatus = true;
-      $("#count-adopted").show();
-      $("#rambo-adopted").show();
+      $("#count-adopted").slideDown();
+      $("#recent-adopted").show();
     } else {
       console.log("Button ID doesn't match any existing animal");
     }
